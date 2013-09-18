aabd099a756814e9cb40fbaa085d6cdb93fde69f
Sam Varner <snick-a-doo@comcast.net>
2013-09-17 21:14:27 -0400

    Added GPL3 license
    Added permission statement to source files.  Added COPYING file which includes
    the full license text.

--- xlmc.py
+++ xlmc.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+# xlmc.py - A graphical front end for the Little Man Computer emulator.
+#
+# Copyright 2013 Sam Varner
+#
+# This file is part of Little Village
+#
+# Little Village is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# Little Village is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Little Village.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import sys
 from gi.repository import Gtk
