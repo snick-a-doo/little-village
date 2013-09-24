b5b7d5cb002c18686cb8299f07214296b060d5e8
Sam Varner <snick-a-doo@comcast.net>
2013-09-23 20:46:43 -0400

    Added errors and warnings.
    Code's getting messy.

--- assemble.py
+++ assemble.py
@@ -24,34 +24,47 @@ import string
 import sys
 
 # Errors:
-#   Unknown mnemonic
+#  * Unknown mnemonic
 #   Wrong number of arguments for opcode
 #   Argument out of range
 #   Undefined label
+#  * Too long
+#  * Label matches mnemonic
+#   Too many errors
 #
 # Warnings:
-#   Unused label
-#   No HLT
+#  * Unused label
+#  * No HLT
+#   Data may be executed as code
+#   Jump to data
+#   Unlabeled data
+
+class Abort: pass
 
 class Assembler:
     # Ignore everything from this string to the end of the line in input files.
     comment = ';'
 
     # Mnemonic/opcode associations.  For convenience 'DAT' is treated as opcode 0.
-    opcodes = { 'HLT':000, 'ADD':100, 'SUB':200, 'STA':300, 'LDA':500, 
-                'BRA':600, 'BRZ':700, 'BRP':800, 'INP':901, 'OUT':902,
-                'DAT':000 }
+    opcodes = { 'DAT':(000, 0, 1), 'HLT':(000, 0, 0), 
+                'ADD':(100, 1, 0), 'SUB':(200, 1, 0), 
+                'STA':(300, 1, 0), 'LDA':(500, 1, 0), 
+                'BRA':(600, 1, 0), 'BRZ':(700, 1, 0), 'BRP':(800, 1, 0),
+                'INP':(901, 0, 0), 'OUT':(902, 0, 0) }
+
+    max_errors = 10
 
     def __init__ (self):
         # The numeric values of labels are defined in a dictionary.  An empty
         # label is zero.
-        self.data = { '':0 }
+        self.data = { '':(0, 0, 1) }
         self.code = []
+        self.has_halt = False
         self.messages = []
         self.n_errors = 0
         self.n_warnings = 0
 
-    def add_message (self, fatal, message, line_number, line):
+    def add_message (self, fatal, message, line_number = -1, line = ''):
         severity = ''
         if fatal:
             self.n_errors += 1
@@ -60,6 +73,9 @@ class Assembler:
             self.n_warnings += 1
             severity = 'Warning'
         self.messages.append ((severity, message, line_number, line))
+        if self.n_errors == self.max_errors:
+            self.add_message (True, '%d errors.  Quitting.' % self.max_errors)
+            raise Abort
 
     def interpret_mnemonics (self, program):
         code = []
@@ -74,57 +90,98 @@ class Assembler:
             n = len (tokens)
             if n > 0:
                 if n > 1 and not self.opcodes.has_key (tokens[0]):
-                    # If the 1st token is not a mnemonic assume it's a label.  Its
-                    # value is the memory location of the instruction, which is the
-                    # same as the number of instructions processed so far.
-                    self.data [tokens[0]] = len (code)
-                    # Remove the label from the token list.
-                    tokens = tokens[1:]
-                    # Interpret the instruction (possibly after label removal).
+                    if not self.opcodes.has_key (tokens [1]):
+                        self.add_message (True, 'Unknown mnemonic', line_number, line)
+                        continue
+                    else:
+                        # If the 1st token is not a mnemonic assume it's a
+                        # label.  Its value is the memory location of the
+                        # instruction, which is the same as the number of
+                        # instructions processed so far.
+                        self.data [tokens[0]] = (len (code), line_number, 0)
+                        # Remove the label from the token list.
+                        tokens = tokens[1:]
+                        # Interpret the instruction (possibly after label removal).
                 try:
-                    code.append (self.translate (tokens))
+                    (op, arg) = self.translate (tokens, line_number, line)
+                    if arg != '' and self.opcodes.has_key (arg):
+                        self.add_message (True, 
+                                          'Label must not be the same as a mnemonic',
+                                          line_number,
+                                          line)
+                        # Clear the argument so we continue examining the code.
+                        arg = ''
+                    code.append ((op, arg))
                 except KeyError:
                     self.add_message (True, 'Unknown mnemonic', line_number, line)
+                if len (code) > 100:
+                    self.add_message (True, 'Program too long', line_number, line)
+                    break
         return code
 
-    def translate (self, instruction):
+    def translate (self, instruction, line_number, line):
+        if instruction[0] == 'HLT':
+            self.has_halt = True
+
         # Get the machine code for the mnemonic
-        op = self.opcodes [instruction[0]]
+        opcode = self.opcodes [instruction[0]]
+        op = opcode[0]
 
         # Use the empty string if there's no argument.
         arg = ''
-        if len (instruction) > 1:
+        n_args = len (instruction[1:])
+        if n_args != opcode[1]:
+                self.add_message (True, 
+                                  ('%s requires %s, %d given'
+                                   % (instruction[0], 
+                                      count (opcode[1], 'argument'),
+                                      n_args)),
+                                  line_number,
+                                  line)
+        elif len (instruction) > 1:
             arg = instruction[1]
         return (op, arg)
 
     def resolve_labels (self, code):
         out = []
         for line in code:
-            # Don't process messages
-            if type (line[0]) == type (''):
-                out.append (line)
-            else:
-                arg = 0
-                try:
-                    # Try to find the value for the (possibly empty) argument label...
-                    arg = self.data [line[1]]
-                except KeyError:
-                    # ...if that fails try to treat the argument as a number.
-                    arg = int (line[1])
-                    # Add the numeric argument to the machine code.
-                out.append (line[0] + arg)
+            arg = 0
+            try:
+                # Try to find the value for the (possibly empty) argument
+                # label...
+                label = line[1]
+                (value, line_n, refs) = self.data [label]
+                arg = value
+                self.data [label] = (value, line_n, refs + 1)
+            except KeyError:
+                # ...if that fails try to treat the argument as a number.
+                arg = int (line[1])
+                # Add the numeric argument to the machine code.
+            out.append (line[0] + arg)
         return out
 
     def assemble (self, program):
-        # The first pass turns the program into an array of tuples.  The 1st
-        # element is the machine instruction for the mnemonic.  The second is a
-        # string which may be empty, a label, or the string represtation of a
-        # numeric argument.
-        code = self.interpret_mnemonics (program)
+        self.has_halt = False
+        try:
+            # The first pass turns the program into an array of tuples.  The 1st
+            # element is the machine instruction for the mnemonic.  The second is a
+            # string which may be empty, a label, or the string represtation of a
+            # numeric argument.
+            code = self.interpret_mnemonics (program)
 
-        # The second pass replaces labels with their addresses and generates the
-        # machine code.
-        self.code = self.resolve_labels (code)
+            # The second pass replaces labels with their addresses and generates the
+            # machine code.
+            self.code = self.resolve_labels (code)
+        except Abort:
+            pass
+        else:
+            for label in self.data:
+                (value, line, refs) = self.data[label]
+                if refs == 0:
+                    self.add_message (False, 'Unused label', line, program [line - 1])
+
+            if not self.has_halt:
+                self.add_message (False, 'No HLT instruction in input')
 
         return self.n_errors == 0
 
@@ -134,7 +191,10 @@ class Assembler:
 
     def write_messages (self, stream = sys.stderr):
         for message in self.messages:
-            stream.write ('%s: %s\n  line %-3d: %s\n' % message)
+            if message[2] < 0:
+                stream.write ('%s: %s\n' % message[:2])
+            else:
+                stream.write ('%s: %s\n  line %-3d: %s\n' % message)
 
         if self.n_errors > 0 or self.n_warnings > 0:
             stream.write ('\n%s, %s\n' 
--- test/test-assemble.py
+++ test/test-assemble.py
@@ -7,6 +7,7 @@ sys.path.append (os.path.abspath ('..'))
 
 import assemble
 import unittest
+import string
 import StringIO
 
 '''
@@ -27,19 +28,102 @@ class Test_Assemble (unittest.TestCase):
         self.assertEqual (self.asm.code, [])
 
     def test_mnemonics (self):
-        program = [ 'ADD', 'SUB', 'STA', 'LDA', 'BRA', 'BRZ', 'BRP',
+        program = [ 'ADD 50', 'SUB 51', 'STA 52', 'LDA 53',
+                    'BRA 01', 'BRZ 02', 'BRP 03',
                     'INP', 'OUT', 'HLT', 'DAT' ]
         self.assertTrue (self.asm.assemble (program))
         self.assertEqual (self.asm.code,
-                          [ 100, 200, 300, 500, 600, 700, 800, 
+                          [ 150, 251, 352, 553, 601, 702, 803, 
                             901, 902, 000, 000 ])
+
     def test_unknown_mnemonic (self):
         # LDA is misspelled LDA
-        program = [ 'ADD', 'SUB', 'STA', 'LAD', 'BRA', 'BRZ', 'BRP',
-                    'INP', 'OUT', 'HLT', 'DAT' ]
+        program = [ 'ADD 50', 'SUB 51', 'STA 52', 'LAD 53', 'HLT' ]
         self.assertFalse (self.asm.assemble (program))
         self.assertEqual (self.messages (),
-                          'Error: Unknown mnemonic\nline 4  : LAD\n1 error, 0 warnings\n')
+                          'Error: Unknown mnemonic\n'
+                          '  line 4  : LAD 53\n'
+                          '\n'
+                          '1 error, 0 warnings\n')
+
+    def test_unused_label (self):
+        program = [ 'ADD 50', 'nothing DAT', 'HLT' ]
+        self.assertTrue (self.asm.assemble (program))
+        self.assertEqual (self.asm.code, [ 150, 000, 000 ])
+        self.assertEqual (self.messages (),
+                          'Warning: Unused label\n'
+                          '  line 2  : nothing DAT\n'
+                          '\n'
+                          '0 errors, 1 warning\n')
+
+    def test_too_long (self):
+        # Avoid the no-halt warning.
+        program = [ 'HLT' ] + [ 'ADD 50' ] * 111
+        self.assertFalse (self.asm.assemble (program))
+        self.assertEqual (self.messages (),
+                          'Error: Program too long\n'
+                          '  line 101: ADD 50\n'
+                          '\n'
+                          '1 error, 0 warnings\n')
+
+    def test_label_is_mnemonic (self):
+        # 'STA SUB' will be read as op=STA arg=SUB
+        program = [ ';comment', '', 'ADD 50', 'STA SUB', 'HLT' ]
+        self.assertFalse (self.asm.assemble (program))
+        # The line number is source file lines.  Comments and blanks are
+        # included in the count.
+        self.assertEqual (self.messages (),
+                          'Error: Label must not be the same as a mnemonic\n'
+                          '  line 4  : STA SUB\n'
+                          '\n'
+                          '1 error, 0 warnings\n')
+
+    def test_no_halt (self):
+        program = [ 'ADD 50', 'SUB 51' ]
+        self.assertTrue (self.asm.assemble (program))
+        self.assertEqual (self.asm.code, [ 150, 251 ])
+        self.assertEqual (self.messages (),
+                          'Warning: No HLT instruction in input\n'
+                          '\n'
+                          '0 errors, 1 warning\n')
+
+    def test_too_many_errors (self):
+        self.asm.max_errors = 4
+        program = [ 'MOO', 'WEE', 'ZIP', 'TOP', 'BAG' ]
+        self.assertFalse (self.asm.assemble (program))
+        messages = string.split (self.messages (), '\n')
+        self.assertEqual (messages[0], 'Error: Unknown mnemonic')
+        self.assertEqual (messages[1], '  line 1  : MOO')
+        self.assertEqual (messages[2], 'Error: Unknown mnemonic')
+        self.assertEqual (messages[3], '  line 2  : WEE')
+        self.assertEqual (messages[4], 'Error: Unknown mnemonic')
+        self.assertEqual (messages[5], '  line 3  : ZIP')
+        self.assertEqual (messages[6], 'Error: Unknown mnemonic')
+        self.assertEqual (messages[7], '  line 4  : TOP')
+        self.assertEqual (messages[8], 'Error: 4 errors.  Quitting.')
+        self.assertEqual (messages[9], '')
+        self.assertEqual (messages[10], '5 errors, 0 warnings')
+
+    def test_not_enough_arguments (self):
+        program = [ 'ADD', 'SUB', 'STA', 'LDA', 'BRA', 'BRZ', 'BRP', 'HLT', 'DAT' ]
+        self.assertFalse (self.asm.assemble (program))
+        messages = string.split (self.messages (), '\n')
+        self.assertEqual (messages[0], 'Error: ADD requires 1 argument, 0 given')
+        self.assertEqual (messages[1], '  line 1  : ADD')
+        self.assertEqual (messages[2], 'Error: SUB requires 1 argument, 0 given')
+        self.assertEqual (messages[3], '  line 2  : SUB')
+        self.assertEqual (messages[4], 'Error: STA requires 1 argument, 0 given')
+        self.assertEqual (messages[5], '  line 3  : STA')
+        self.assertEqual (messages[6], 'Error: LDA requires 1 argument, 0 given')
+        self.assertEqual (messages[7], '  line 4  : LDA')
+        self.assertEqual (messages[8], 'Error: BRA requires 1 argument, 0 given')
+        self.assertEqual (messages[9], '  line 5  : BRA')
+        self.assertEqual (messages[10], 'Error: BRZ requires 1 argument, 0 given')
+        self.assertEqual (messages[11], '  line 6  : BRZ')
+        self.assertEqual (messages[12], 'Error: BRP requires 1 argument, 0 given')
+        self.assertEqual (messages[13], '  line 7  : BRP')
+        self.assertEqual (messages[14], '')
+        self.assertEqual (messages[15], '7 errors, 0 warnings')
 
 if __name__ == '__main__':
     unittest.main ()
